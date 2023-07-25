50a90b39f6de4164feee0945e25b489a4ed7bac5
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-26 00:50:12 +0800

    add: add GameNotFound and NotFound views

--- app/src/routes/router.ts
+++ app/src/routes/router.ts
@@ -1,21 +1,34 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import GameView from "../views/GameView.vue";
 import HomeView from "../views/HomeView.vue";
+import NotFound from "../views/NotFound.vue";
+import GameNotFound from "../views/GameNotFound.vue";
 
 const routes = [
-  {
-    path: '/',
-    component: HomeView,
-  },
-  {
-    path: "/game",
-    component: GameView,
-  },
+	{
+		name: "home",
+		path: '/',
+		component: HomeView,
+	},
+	{
+		path: "/game/:gameId",
+		component: GameView,
+	},
+	{
+		path: '/game/:pathMatch(.*)*',
+		name: "GameNotFound",
+		component: GameNotFound
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: NotFound
+	}
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+	history: createWebHashHistory(),
+	routes,
 });
 
 export default router;
---
+++ app/src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+	<main class="d-flex align-items-center justify-content-center">
+		<h1>Page Not Found :(</h1>
+	</main>
+</template>
+
+<style scoped>
+main {
+	height: 80vh;
+	width: 100vw;
+}
+</style>
