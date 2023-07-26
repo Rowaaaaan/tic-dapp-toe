f15a534f7b4a533b235188c4ee5257f4d5055dd3
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-26 16:12:33 +0800

    change game.ts to use the anchor program in making game moves
    This commit removes the dummy behavior in the game, and tries to link the
    program IDL to the frontend. It also adds some validation for the game public key
    and redirecting when it is invalid

--- app/src/views/scripts/game.ts
+++ app/src/views/scripts/game.ts
@@ -1,22 +1,63 @@
-import { ref, reactive } from "vue";
+import { ref } from "vue";
+import type { Ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { PublicKey, PUBLIC_KEY_LENGTH } from "@solana/web3.js";
+import base58 from "bs58";
+
+import { useWorkspace } from "../../stores/workspace";
 import { Board, Tile } from "./game-objects";
 
-export default {
-  setup() {
-    const playerOneTurn = ref(true);
-    const board = reactive(new Board());
+const { program } = useWorkspace();
 
-    const setTile = (tile: Tile) => {
-      console.log(tile);
-      if (!tile.isSet()) {
-        playerOneTurn.value
-          ? board.setTile("O", tile)
-          : board.setTile("X", tile);
-        playerOneTurn.value = !playerOneTurn.value;
-      } else {
-        console.log("Error: Tile already set!");
-      }
-    };
-    return { board, setTile };
-  },
+
+export default {
+	setup() {
+		const route = useRoute();
+		const router = useRouter();
+
+		const gameId = route.params.gameId as string;
+		let gamePubKey: PublicKey;
+
+		try {
+			gamePubKey = new PublicKey(gameId);
+		} catch (e) {
+			router.push({ name: "GameNotFound" });
+			console.error(`Game ID: ${gameId}`);
+			console.error(`Error getting pubkey from game id. Error: ${e}`);
+		}
+
+		const gameState: Ref<any> = ref(null);
+		const updateGameState = async () => {
+			if (program == null) {
+				console.error("Game Program or provider does not exist");
+				return;
+			}
+			await program?.value.account.game.fetch(gamePubKey)
+				.then((state) => {
+					gameState.value = state;
+				})
+				.catch((e) => {
+					console.error(`Failed to get game state. Error: ${e}`);
+				});
+		}
+
+		const board: Ref<Board> = ref(new Board());
+		const updateBoardState = () => {
+			board.value ??= gameState.value.board;
+			console.log(board.value);
+		}
+
+		const setTile = (tile: Tile) => {
+			console.log(tile);
+			program?.value.methods.play({ row: tile.x, column: tile.y });
+		};
+
+		const onSetTile = (tile: Tile) => {
+			setTile(tile);
+			updateGameState();
+			updateBoardState();
+		};
+
+		return { gameId, board, onSetTile };
+	},
 };
