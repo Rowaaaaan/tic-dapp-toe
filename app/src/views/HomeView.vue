eb2bd0dd7ded518a895dfbee915e8b4e5b09dd16
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-26 15:46:52 +0800

    fix HomeView UI
    Remove the openGames part of the UI, because I am not implementing that
    any time soon.
    Change the UI so that it only asks for player 2's public key to create a game,
    or join an existing game by entering the game's public key.
    
    TODO: Fix walletStore not being set by initWallet() from solana-wallets-vue

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,23 +1,23 @@
 <template>
-	<div class="app-wrapper d-flex flex-column align-items-center">
+	<div class="app-wrapper d-flex flex-column align-items-center justify-content-center">
 		<h1>Play Tic Dapp Toe</h1>
 		<div v-if="!isWalletConnected">
 			<p class="text-xl-b">Connect Wallet to continue</p>
 		</div>
-		<h2 class="text-center">Open games:</h2>
-		<div v-if="openGames.length > 0" class="open-games-list">
-			<div v-for="game in openGames" class="game d-flex flex-column p-2 m-2">
-				<div class="d-flex justify-content-between">
-					<p>{{ game.hash }}</p>
-					<button class="btn btn-primary" @click="onJoinGame">Join Game</button>
-				</div>
+		<div class="d-flex flex-column align-items-center">
+			<p class="fs-3">To join game, enter game public key:</p>
+			<div class="d-flex flex-row">
+				<input ref="playerTwoPubKey" class="game-key-input" placeholder="Game public key here" />
+				<button class="btn btn-primary m-2" @click="onJoinGame">Join</button>
 			</div>
 		</div>
-		<div v-else>
-			<p class="fs-3">There are currently no open games</p>
-		</div>
-		<div>
-			<button class="btn btn-success" @click="onCreateGame">Create New Game</button>
+		<div class="d-flex flex-column align-items-center">
+			<p class="fs-3">Or</p>
+			<p class="fs-3">Create Game</p>
+			<div class="d-flex justify-content-between">
+				<input ref="gameIdRef" class="game-key-input" placeholder="Game public key here" />
+				<button class="btn btn-success m-2" @click="onCreateGame">Create</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -25,6 +25,11 @@
 <script src="./scripts/home.ts" lang="ts" />
 
 <style scoped>
+.app-wrapper {
+	height: 88vh;
+	width: 100vw;
+}
+
 .open-games-list {
 	min-width: 30vw;
 }
@@ -33,5 +38,9 @@
 	border: 2px solid black;
 	border-radius: 5px;
 	justify-content: center;
+}
+
+.game-key-input {
+	width: 25rem;
 }
 </style>
