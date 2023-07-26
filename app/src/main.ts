81225cd6deee167218adc1e2e9b90ccd2593d144
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-26 16:32:44 +0800

    fix main.ts initialize wallet on webapp startup

--- app/src/main.ts
+++ app/src/main.ts
@@ -7,31 +7,27 @@ import { createPinia } from "pinia";
 
 import { initWallet } from "solana-wallets-vue";
 
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
 import {
 	PhantomWalletAdapter,
 	SlopeWalletAdapter,
 	SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+const walletOptions = {
+	wallets: [
+		new PhantomWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+		new SlopeWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+		new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+	],
+	autoConnect: true,
+};
 
-function initializeWallet() {
-	const walletOptions = {
-		wallets: [
-			new PhantomWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-			new SlopeWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-			new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-		],
-		autoConnect: true,
-	};
+console.log(`Wallet options: `);
+console.log(walletOptions);
 
-	console.log(`Wallet options: `);
-	console.log(walletOptions);
-
-	initWallet(walletOptions);
-}
-
-initializeWallet();
+initWallet(walletOptions);
 
 import App from "@/App.vue";
 
