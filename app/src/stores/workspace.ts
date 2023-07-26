7b670c3644441f29ab965b8c09305620c73347e7
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-26 15:50:46 +0800

    make workspace import anchor program idl and init wallet store

--- app/src/stores/workspace.ts
+++ app/src/stores/workspace.ts
@@ -1,7 +1,7 @@
 import { computed } from "vue";
-import { useAnchorWallet } from "solana-wallets-vue";
+import { useWallet, useAnchorWallet } from "solana-wallets-vue";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
-import type { Idl } from "@coral-xyz/anchor";
+import type { TicTacToe } from "../idl/tic_tac_toe";
 import idl from "../idl/tic_tac_toe.json";
 import {
 	PublicKey,
@@ -9,20 +9,45 @@ import {
 	clusterApiUrl,
 } from "@solana/web3.js";
 
+import { initWallet } from "solana-wallets-vue";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+	PhantomWalletAdapter,
+	SlopeWalletAdapter,
+	SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+const walletOptions = {
+	wallets: [
+		new PhantomWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+		new SlopeWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+		new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+	],
+	autoConnect: true,
+};
 
 export const useWorkspace = () => {
+	initWallet(walletOptions);
 	// @ts-ignore
-	const IDL: Idl = idl;
+	const IDL: TicTacToe = idl;
+	const programAddress = process.env.PROGRAM_ADDRESS ?? "CwnZBvhPLva1bSUiunhvatsBAL4o7LspALj1BgQpa3AP";
+	console.log(`Program address: ${programAddress}`);
 	const conn = new Connection(clusterApiUrl("devnet", true), "confirmed");
-	const wallet = useAnchorWallet();
 
-	console.log("Wallet data: ");
-	console.log(wallet.value);
+	// Provider wallet is purely for creating a provider, which we need
+	// to create the program
+	const providerWallet = useAnchorWallet();
 
-	console.log("Wallet Connected: ");
-	console.log(wallet.value?.publicKey.toBase58());
+	// useWallet() is better for using in UI
+	const userWallet = useWallet();
 
-	const programID = new PublicKey(idl.metadata.address);
+	console.log("User wallet data: ");
+	console.log(userWallet);
+
+	console.log("Provider Wallet data: ");
+	console.log(providerWallet);
+
+	const programID = new PublicKey(programAddress);
 	console.log("IDL data:");
 	console.log(IDL);
 
@@ -30,18 +55,18 @@ export const useWorkspace = () => {
 	const commitment = "confirmed";
 
 	const provider = computed(() => {
-		return wallet.value
-			? new AnchorProvider(conn, wallet.value, {
+		return providerWallet.value
+			? new AnchorProvider(conn, providerWallet.value, {
 				preflightCommitment: preflightCommitment,
 				commitment: commitment,
 			})
 			: undefined;
 	});
 
-	const program = provider.value ? computed(() => new Program(IDL, programID, provider.value)) : null;
+	const program = provider.value ? computed(() => new Program<TicTacToe>(IDL, programID, provider.value)) : null;
 
 	return {
-		wallet,
+		userWallet,
 		conn,
 		provider,
 		program,
