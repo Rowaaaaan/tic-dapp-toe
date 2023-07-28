d016eac7d65ee2b1b963c41b9a7f7f77f50032f6
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-29 00:10:20 +0800

    remove wallet init functions from main.ts
    This commit moves all the wallet init functions to workspace.ts, because otherwise,
    none of the other components will be able to see the wallet that's connected.
    
    For some reason, using SolanaWallets as a plugin doesn't seem to work either,
    and the wallet isn't visible to any of the other components.

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,30 +5,6 @@ import router from "@/routes/router";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import { initWallet } from "solana-wallets-vue";
-
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-
-import {
-	PhantomWalletAdapter,
-	SlopeWalletAdapter,
-	SolflareWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
-
-const walletOptions = {
-	wallets: [
-		new PhantomWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-		new SlopeWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-		new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
-	],
-	autoConnect: true,
-};
-
-console.log(`Wallet options: `);
-console.log(walletOptions);
-
-initWallet(walletOptions);
-
 import App from "@/App.vue";
 
 const app = createApp(App);
