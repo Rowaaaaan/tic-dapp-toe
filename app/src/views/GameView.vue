c816f81fbf0bd209a1b10b5cbaafb9dbdae0838d
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-29 01:14:08 +0800

    implement game ui functionality
    This commit adds actual functionality to the game UI. A lot of hacky stuff was done
    to fetch data from the RPC.
    
    TODO: Fix issue with provider wallet and player wallet not connecting to app
    if user creates a new game without refreshing.

--- app/src/views/GameView.vue
+++ app/src/views/GameView.vue
@@ -1,11 +1,43 @@
 <template>
 	<main class="d-flex justify-content-center">
-		<div class="container game-wrapper">
-			<h3 class="text-center">Game ID: {{ gameId }}</h3>
-			<div v-for="row in board.rows" class="row">
-				<div v-for="tile in row" class="tile col d-flex justify-content-center align-items-center"
-					@click="onSetTile(tile)">
-					<p :class="playerOneTurn ? 'player-one' : 'player-two'">{{ tile.value }}</p>
+		<div class="d-flex flex-wrap align-items-center justify-content-center game-wrapper">
+			<div class="board d-flex flex-column justify-content-center align-items-center">
+				<div v-for="row of board.rows" class="row">
+					<div v-for="tile of row" :key="tile" class="tile col d-flex justify-content-center align-items-center"
+						@click="onSetTile(tile)">
+						<p>{{ tile.value }}</p>
+					</div>
+				</div>
+			</div>
+			<div class="game-stats d-flex flex-column justify-content-center border border-2 border-dark rounded-3 p-3">
+				<button class="btn btn-warning" @click="onRefreshBoard">Refresh board</button>
+				<div class="d-flex justify-content-between">
+					<p class="fw-bold">Game ID:</p>
+					<p>{{ gameId }}</p>
+				</div>
+				<div class="d-flex justify-content-between">
+					<p class="fw-bold">Turn number:</p>
+					<p>{{ turn }}</p>
+				</div>
+				<div class="d-flex justify-content-between">
+					<p class="fw-bold">Player 1 [X]:</p>
+					<p>{{ playerOnePubKey }}</p>
+				</div>
+				<div class="d-flex justify-content-between">
+					<p class="fw-bold">Player 2 [O]:</p>
+					<p>{{ playerTwoPubKey }}</p>
+				</div>
+				<div v-if="winner != null">
+					<p class="fs-2 fw-bold">
+						{{ playerOneWin ? "Player 1" :
+							"Player 2" }}
+						wins!
+					</p>
+				</div>
+				<div v-else class="d-flex justify-content-between">
+					<p class="fw-bold">
+						{{ playerTurn ? "Your turn" : "Opponent's turn" }}
+					</p>
 				</div>
 			</div>
 		</div>
@@ -15,6 +47,11 @@
 <script src="./scripts/game.ts" lang="ts" />
 
 <style scoped>
+.board {
+	height: 550px;
+	width: 500px;
+}
+
 .tile {
 	border: 1px solid;
 	height: 150px;
--- app/src/views/scripts/game.ts
+++ app/src/views/scripts/game.ts
@@ -1,14 +1,16 @@
-import { ref } from "vue";
+import { ref, watch, onMounted } from "vue";
 import type { Ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { PublicKey, PUBLIC_KEY_LENGTH } from "@solana/web3.js";
-import base58 from "bs58";
+import { PublicKey } from "@solana/web3.js";
+import * as anchor from "@coral-xyz/anchor";
 
-import { useWorkspace } from "../../stores/workspace";
+import { useWorkspace, validateProgram } from "../../stores/workspace";
+
+import { useGameManager } from "./game-manager";
 import { Board, Tile } from "./game-objects";
 
-const { program } = useWorkspace();
-
+const { program, provider, wallet } = useWorkspace();
+const { retrieveGameSession } = useGameManager();
 
 export default {
 	setup() {
@@ -17,47 +19,160 @@ export default {
 
 		const gameId = route.params.gameId as string;
 		let gamePubKey: PublicKey;
+		const gameState: Ref<any> = ref();
 
+		// Check if a public key can be generated from the game id passed in
 		try {
 			gamePubKey = new PublicKey(gameId);
+
 		} catch (e) {
-			router.push({ name: "GameNotFound" });
 			console.error(`Game ID: ${gameId}`);
 			console.error(`Error getting pubkey from game id. Error: ${e}`);
+			router.push({ name: "GameNotFound" });
 		}
 
-		const gameState: Ref<any> = ref(null);
-		const updateGameState = async () => {
-			if (program == null) {
-				console.error("Game Program or provider does not exist");
-				return;
+		// Get the keys as a Uint8Array to be able to convert it to a string.
+		// Otherwise, the comparison will fail for some reason.
+		const playerOnePubKey: Ref<Uint8Array | null> = ref(null);
+		const playerTwoPubKey: Ref<Uint8Array | null> = ref(null);
+		const winner: Ref<Uint8Array | null> = ref(null);
+		const turn: Ref<number> = ref(0);
+		const playerTurn: Ref<boolean> = ref(false);
+		const playerOneWin: Ref<boolean> = ref(false);
+		watch(gameState, () => {
+			updateBoardState();
+			playerOnePubKey.value = gameState.value?.players[0] as Uint8Array;
+			playerTwoPubKey.value = gameState.value?.players[1] as Uint8Array;
+			winner.value = gameState.value?.state.won?.winner as Uint8Array;
+			turn.value = gameState.value?.turn;
+			playerTurn.value = (playerPubKey.value?.toString() === playerOnePubKey.value.toString())
+				? (turn.value % 2 === 1)
+				: (turn.value % 2 === 0);
+			playerOneWin.value = (playerOnePubKey.value?.toString() === winner.value?.toString());
+		});
+
+		onMounted(async () => {
+			useWorkspace();
+			gameState.value = await retrieveGameSession(gamePubKey);
+			console.log("GameView component mounted");
+		});
+
+		let playerWallet: anchor.AnchorProvider = (program?.value?.provider as anchor.AnchorProvider);
+		const playerPubKey: Ref<PublicKey | null> = wallet.publicKey;
+		const setPlayerWalletData = () => {
+			try {
+				playerWallet = (program?.value?.provider as anchor.AnchorProvider);
+				console.log("Player wallet data");
+				console.log(playerWallet);
+			} catch (e) {
+				console.log(`Failed to get provider wallet. Error: ${e}`);
 			}
-			await program?.value.account.game.fetch(gamePubKey)
-				.then((state) => {
-					gameState.value = state;
-				})
-				.catch((e) => {
-					console.error(`Failed to get game state. Error: ${e}`);
-				});
-		}
-
-		const board: Ref<Board> = ref(new Board());
-		const updateBoardState = () => {
-			board.value ??= gameState.value.board;
-			console.log(board.value);
-		}
-
-		const setTile = (tile: Tile) => {
-			console.log(tile);
-			program?.value.methods.play({ row: tile.x, column: tile.y });
 		};
 
-		const onSetTile = (tile: Tile) => {
+		watch(program, async () => {
+			setPlayerWalletData();
+			gameState.value = await retrieveGameSession(gamePubKey);
+			updateBoardState();
+		});
+
+		watch(provider, () => {
+			console.log("Provider changed!");
+			console.log(provider);
+		})
+
+		const board: Ref<Board> = ref(new Board());
+
+		// Horribly inefficient method of updating board state from RPC data
+		const updateBoardState = () => {
+			try {
+				console.log("Tile states");
+				let x = 0;
+				let y = 0;
+				for (let row of gameState.value.board) {
+					y = 0;
+					for (let tile of row) {
+						if (tile != null) {
+							if (tile.o != null) {
+								board.value.setTile("O", { x, y });
+							} else if (tile.x != null) {
+								board.value.setTile("X", { x, y });
+							}
+						}
+						y++;
+					}
+					x++;
+				}
+			} catch (e) {
+				console.log(`Failed to get tile states. Error: ${e}`)
+			}
+		}
+
+		const updateGameState = async () => {
+			let returnState: any;
+			try {
+				validateProgram(program?.value);
+				await program?.value?.account.game.fetch(gamePubKey)
+					.then((state) => {
+						returnState = state;
+					})
+					.catch((e) => {
+						console.error(`Failed to get game state. Error: ${e}`);
+					});
+			} catch (e) {
+				console.log(`Failed to update game state. Error ${e}`);
+			}
+			return returnState;
+		}
+
+		const setTile = async (tile: Tile) => {
+			try {
+				validateProgram(program?.value);
+				if (playerPubKey.value == null) return;
+				console.log("Player wallet data");
+				console.log(playerWallet);
+				program?.value?.methods
+					.play({ row: tile.row, column: tile.column })
+					.accounts({
+						player: playerPubKey.value,
+						game: gamePubKey
+					})
+					.signers(playerWallet ? [] : [playerWallet])
+					.rpc()
+					.then(txHash => {
+						console.log(`Tile set! Hash: ${txHash}`);
+						updateGameState();
+					})
+					.catch(e => {
+						console.log(`Failed to set tile. Error: ${e}`);
+					});
+			} catch (e) {
+				console.log(`Failed to set tile. Error: ${e}`);
+			}
+		};
+
+		const onSetTile = async (tile: Tile) => {
+			if (playerWallet == null) {
+				setPlayerWalletData();
+			}
 			setTile(tile);
-			updateGameState();
+		};
+
+		const onRefreshBoard = () => {
 			updateBoardState();
 		};
 
-		return { gameId, board, onSetTile };
+		return {
+			playerPubKey,
+			playerOnePubKey,
+			playerTwoPubKey,
+			playerOneWin,
+			playerTurn,
+			winner,
+			gameId,
+			turn,
+			board,
+			onSetTile,
+			onRefreshBoard
+		};
 	},
 };
