a0b6ac4d1953e5002060e7786a0663b0889ee9d0
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-29 00:35:53 +0800

    change game objects to work with rpc account data

--- app/src/views/scripts/game-objects.ts
+++ app/src/views/scripts/game-objects.ts
@@ -1,45 +1,37 @@
 export class Tile {
-  value: string | null;
-  x: number;
-  y: number;
+	value: string;
+	row: number;
+	column: number;
 
-  constructor(x?: number, y?: number) {
-    this.value = null;
-    this.x = x || 0;
-    this.y = y || 0;
-  }
+	constructor(row?: number, column?: number) {
+		this.value = "";
+		this.row = row || 0;
+		this.column = column || 0;
+	}
 
-  get() {
-    return this.value;
-  }
-
-  set(value: string) {
-    this.value = value;
-  }
-
-  isSet() {
-    return this.value ? true : false;
-  }
+	set(value: string) {
+		this.value = value;
+	}
 }
 
 export class Board {
-  rows: Array<Array<Tile>>;
+	rows: Array<Array<Tile>>;
 
-  constructor() {
-    let tempRows: Array<Array<Tile>> = [];
-    for (let i = 0; i < 3; i++) {
-      let row = new Array<Tile>();
-      for (let j = 0; j < 3; j++) {
-        row.push(new Tile(i, j));
-      }
-      tempRows.push(row);
-    }
-    this.rows = tempRows;
-  }
+	constructor() {
+		let tempRows: Array<Array<Tile>> = [];
+		for (let i = 0; i < 3; i++) {
+			let row = new Array<Tile>();
+			for (let j = 0; j < 3; j++) {
+				row.push(new Tile(i, j));
+			}
+			tempRows.push(row);
+		}
+		this.rows = tempRows;
+	}
 
-  setTile(value: string, tile?: Tile, x?: number, y?: number) {
-    let posX = x || tile?.x || 0;
-    let posY = y || tile?.y || 0;
-    this.rows[posX][posY].set(value);
-  }
+	setTile(value: string, pos?: { x: number, y: number }, tile?: Tile) {
+		let posX = tile?.row || pos?.x || 0;
+		let posY = tile?.column || pos?.y || 0;
+		this.rows[posX][posY].set(value);
+	}
 }
