8e3b3fe6fe2d0cab834a5ebdd5fe16fdf9190e9b
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2023-07-30 15:46:55 +0800

    add buffer polyfill to project and import in main.ts
    This commit adds feross's buffer module.
    Without this module, the build version produced by `npm run build` will ship a
    version that will throw the error "Uncaught ReferenceError: Buffer is not defined",
    whenever any web3 function is called. I don't know why it does this, but adding
    this module and importing it into the entry script fixes that issue.

--- app/src/main.ts
+++ app/src/main.ts
@@ -5,6 +5,22 @@ import router from "@/routes/router";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+/**
+ * Imports a buffer polyfill
+ *
+ * Required for the build version to have a buffer that web3 can use.
+ * npm run dev will work just fine, because it uses the browser window's
+ * buffer to do stuff.
+ *
+ * npm run build && npm run preview will ship a version that doesn't work
+ * because it will throw "Error: Buffer not defined"
+ *
+ * I don't know why else this would work, but it does, and I don't want to
+ * touch it anymore.
+ */
+import { Buffer } from "buffer";
+globalThis.Buffer = Buffer;
+
 import App from "@/App.vue";
 
 const app = createApp(App);
